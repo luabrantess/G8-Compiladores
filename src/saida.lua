5948b9ced9fa13e9d492270fe07e6bb985286437
samarawwleticia <samaradddsantos@gmail.com>
2026-06-22 12:23:36 -0300

    Remove /site do versionamento

---
+++ src/saida.lua
@@ -0,0 +1,17 @@
+-- Codigo Lua gerado automaticamente pelo compilador C para Lua
+
+function dobrar(x)
+    return (x * 2)
+end
+
+function triplicar(x)
+    return (x * 3)
+end
+
+function main()
+    local a = dobrar(5)
+    local b = triplicar(a)
+    return 0
+end
+
+main()
